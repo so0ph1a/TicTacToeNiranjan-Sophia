dcb07caed5ed4feb0cca27600234df46627992b3
Sophia Lai <travelingsophial@gmail.com>
2026-03-09 10:33:36 -0400

    Update Board.java

--- tictactoe/src/tictactoe/Board.java
+++ tictactoe/src/tictactoe/Board.java
@@ -55,7 +55,11 @@ public class Board
     {
     	for(int row = 0; row > grid.length; row++)
     	{
-    		
+    		for(int col = 0; col < grid[0].length; col++)
+    		{
+    			System.out.print(grid[row][col] + " ");
+    		}
+    		System.out.println();
     	}
     	
     }
