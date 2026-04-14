df5f8c7ee57eed4db8bfb74b0c0a3c0d22abda23
Niranjan7-max <ninjaram2010@gmail.com>
2026-04-14 13:57:41 -0400

    Update GameLogic.java

--- TicTacToe/src/tictactoe/GameLogic.java
+++ TicTacToe/src/tictactoe/GameLogic.java
@@ -5,8 +5,42 @@ public class GameLogic
 
 	public boolean checkWin(Board board, char player)
     {
+    	for(int i = 0; i < 3; i++)
+    	{
+    		if(board.getCell(i, 0) == player && board.getCell(i, 1) == player
+    		   && board.getCell(i, 2) == player)
+    		{
+    			return true;
+    		}
+    	}
     	
+    	for(int i = 0; i < 3; i++)
+    	{
+    		if(board.getCell(0, i) == player && board.getCell(1, i) == player && board.getCell(2, i) == player)
+    		{
+    			return true;
+    		}
+    				
+    	}
+    	
+    	for(int i = 0; i < 3; i++)
+    	{
+    		if(board.getCell(0, 0) == player && board.getCell(1, 1) == player && board.getCell(2, 2) == player)
+    		{
+    			return true;
+    		}
+    	}
+    	
+    	for(int i = 0; i < 3; i++)
+    	{
+    		if(board.getCell(0, 2) == player && board.getCell(1, 1) == player && board.getCell(2, 0) == player)
+    		{
+    			return true;
+    		}
+    	}
+    	return false;
     }
+	
 
 	
 	
